2c66fcf278964a10dd95bf14c06039e4cda343c5
Davide Pulci <davide.pulci@gmail.com>
2022-03-03 16:11:16 +0100

    fatte domande all'utente

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('JS OK')
+// chiedere nome all'utente
+const name = prompt('quale è il tuo nome?');
+
+// chiedere cognome all'utente
+const surname = prompt('quale è il tuo cognome?');
+
+// chiedere il colore preferito all'utente
+const favouriteColor = prompt('quale è il tuo colore preferito?');
+
