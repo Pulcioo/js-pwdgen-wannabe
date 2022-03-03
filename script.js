6d85a616d3a3d5e89b26fe8800bf079595326cdd
Davide Pulci <davide.pulci@gmail.com>
2022-03-03 16:25:42 +0100

    aggiunti in console i dati inviati dall'utente

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // chiedere nome all'utente
 const name = prompt('quale è il tuo nome?');
 
+console.log(name)
+
 // chiedere cognome all'utente
 const surname = prompt('quale è il tuo cognome?');
+
+console.log(surname)
 
 // chiedere il colore preferito all'utente
 const favouriteColor = prompt('quale è il tuo colore preferito?');
 
+console.log(favouriteColor)
+
+// creo una password con i dati dell'utente piu il numero 21
